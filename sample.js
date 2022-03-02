5d363af7cc27d89c75904b84fdd29cf9c4b9885e
w5cao <w5cao@ucsd.edu>
2022-03-01 16:20:57 -0800

    get selected word

--- sample.js
+++ sample.js
@@ -2,21 +2,39 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
+
 // The onClicked callback function.
 function onClickHandler(info, tab) {
-    // TODO: change this 
-    console.log("item " + info.menuItemId + " was clicked");
-    console.log("info: " + JSON.stringify(info));
-    console.log("tab: " + JSON.stringify(tab));
+    // Get selected text 
+    var text = info["selectionText"]
+    console.log("WORD: " + text);
+    
+    // Get stored muted words
+    chrome.storage.local.get({ mutedWords: [] }, // default value if not defined yet
+                function (items) {
+                    if (items.css) {
+                        textarea.value = items.css;
+                        message('Loaded saved CSS.');
+                    }
+                });
+    
+    console.log(JSON.stringify(mutedWords));
 };
 
-chrome.contextMenus.onClicked.addListener(onClickHandler); // Add listener
+
+chrome.contextMenus.onClicked.addListener(onClickHandler); 
 
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function () {
-    // Create one test item for each context type.
     var context = "selection";
-
+    var title = "Test '" + context + "' menu item";
+    var id = chrome.contextMenus.create({
+        "title": title, "contexts": [context],
+        "id": "context" + context
+    });
+    console.log("'" + context + "' item:" + id);
+    
     var title = "Test '" + context + "' menu item";
     var id = chrome.contextMenus.create({
         "title": title, "contexts": [context],
